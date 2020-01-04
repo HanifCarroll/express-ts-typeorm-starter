f4f883d15bc98a14a3aebbe9f28ab140d7c0d6c1
Hanif Carroll <HanifCarroll@gmail.com>
2020-01-03 21:50:07 -0500

    Add subscriber folder.  Change ormconfig.

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,24 +1,24 @@
 module.exports = {
-  "type": process.env.DB_TYPE,
-  "host": process.env.DB_HOST,
-  "port": process.env.DB_PORT,
-  "username": process.env.DB_USER,
-  "password": process.env.DB_PASSWORD,
-  "database": process.env.DB_NAME,
-  "synchronize": true,
-  "logging": true,
-   "entities": [
-      "src/typeorm/entity/*.ts"
+  'type': process.env.DB_TYPE,
+  'host': process.env.DB_HOST,
+  'port': process.env.DB_PORT,
+  'username': process.env.DB_USER,
+  'password': process.env.DB_PASSWORD,
+  'database': process.env.DB_NAME,
+  'synchronize': true,
+  'logging': true,
+   'entities': [
+      'src/typeorm/entity/*.ts'
    ],
-   "migrations": [
-      "src/typeorm/migration/*.ts"
+   'migrations': [
+      'src/typeorm/migration/*.ts'
    ],
-   "subscribers": [
-      "src/subscriber/**/*.ts"
+   'subscribers': [
+      'src/subscriber/**/*.ts'
    ],
-   "cli": {
-      "entitiesDir": "src/entity",
-      "migrationsDir": "src/migration",
-      "subscribersDir": "src/subscriber"
+   'cli': {
+      'entitiesDir': 'src/typeorm/entity',
+      'migrationsDir': 'src/typeorm/migration',
+      'subscribersDir': 'src/typeorm/subscriber'
    }
 };
