ee6f3fedac3627cf8e7ded4e66e95711df6933ef
Hanif Carroll <HanifCarroll@gmail.com>
2020-01-03 19:57:40 -0500

    Rename user and auth routes import.  Rename users folder to user.

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,11 @@
-import { Router, Request, Response } from "express";
-import auth from "./auth";
-import user from "./user";
+import { Router } from 'express';
+import authRoutes from './auth/authRoutes';
+import userRoutes from './user/userRoutes';
 
 const routes = Router();
 
-routes.use("/auth", auth);
-routes.use("/user", user);
+routes.use('/auth', authRoutes);
+routes.use('/user', userRoutes);
 routes.use('/', (req, res) => res.json({ success: 'hooray!' }));
 
 export default routes;
